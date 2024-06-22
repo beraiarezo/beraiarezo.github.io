ac6074b115b57801f77a99f652e6cc0abe5d06d5
rezoberaia <beraiarezo@gmail.com>
2024-06-22 14:53:06 +0400

    fix error

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,16 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Failed to find the root element to mount to");
+}
